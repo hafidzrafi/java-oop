cdcda6a9a8dcd06e45a2a36787cb90e6ac771900
hafidz rafi <sapineptwn@gmail.com>
2026-09-04 09:12:56 +0700

    refactor(assignments): format discount value as a percentage in Barang display output

--- jobsheet/jobsheet02/assignments/Barang.java
+++ jobsheet/jobsheet02/assignments/Barang.java
@@ -14,7 +14,7 @@ public class Barang {
         System.out.println("Kode Barang       : " + kode);
         System.out.println("Nama Barang       : " + namaBarang);
         System.out.println("Harga Dasar       : " + hargaDasar);
-        System.out.println("Diskon            : " + diskon);
+        System.out.println("Diskon            : " + (diskon * 100) + "%");
         System.out.println("Harga Jual        : " + hitungHargaJual());
     }
 }
