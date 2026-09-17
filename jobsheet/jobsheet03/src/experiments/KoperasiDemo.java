5b0405fbbbc690368ebd1e1946a3415e4fb8d637
hafidz rafi <sapineptwn@gmail.com>
2026-09-17 19:58:39 +0700

    feat(jobsheet03):  complete experiment 3 Anggota and KoperasiDemo classes for koperasi experiment

---
+++ jobsheet/jobsheet03/src/experiments/KoperasiDemo.java
@@ -0,0 +1,14 @@
+package jobsheet.jobsheet03.src.experiments;
+
+public class KoperasiDemo {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota();
+        anggota1.setNama("Iwan");
+        anggota1.setAlamat("Suhat 10");
+        anggota1.setor(100000);
+        System.out.println("Simpanan " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+        
+        anggota1.pinjam(5000);
+        System.out.println("Pinjaman " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+    }
+}
