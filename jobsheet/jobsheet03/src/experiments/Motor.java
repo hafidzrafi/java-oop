bed5f9ceb1be518096a9ff14f40c9a72b74e0fdd
hafidz rafi <sapineptwn@gmail.com>
2026-09-17 19:48:38 +0700

    feat(jobsheet03): complete experiment 2 encapsulate Motor attributes

--- jobsheet/jobsheet03/src/experiments/Motor.java
+++ jobsheet/jobsheet03/src/experiments/Motor.java
@@ -1,8 +1,38 @@
 package jobsheet.jobsheet03.src.experiments;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+    private int maxKecepatan = 100;
+
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn && kecepatan < maxKecepatan) {
+            kecepatan += 5;
+        } else if (kontakOn && kecepatan >= maxKecepatan) {
+            System.out.println("Kecepatan tidak bisa bertambah karena kecepatan sudah mencapai batas maksimal\n");
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin off!!\n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn && kecepatan > 0) {
+            kecepatan -= 5;
+        } else if (kontakOn && kecepatan <= 0) {
+            System.out.println("Kecepatan tidak bisa berkurang karena kecepatan sudah mencapai batas minimal\n");
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin off!!\n");
+        }
+    }
 
     public void printStatus() {
         if (kontakOn) {
