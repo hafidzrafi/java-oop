a2cfc2098eed431c2d7be4f8ef17ad5ae7d9d303
hafidz rafi <sapineptwn@gmail.com>
2026-09-24 15:19:41 +0700

    feat(jobshet04): complete experiment 3

---
+++ jobsheet/jobsheet04/src/experiments/Main.java
@@ -0,0 +1,26 @@
+package experiments;
+
+public class Main {
+    private String nama;
+    private int a;
+    public Main(String nama, int a) {
+        this.nama = nama;
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
