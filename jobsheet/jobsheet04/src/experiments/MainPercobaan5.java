0aae9a9d740912c8a60bbad7a7393ec254b5a461
hafidz rafi <sapineptwn@gmail.com>
2026-09-24 20:03:35 +0700

    feat(jobsheet04): complete experiment 5

---
+++ jobsheet/jobsheet04/src/experiments/MainPercobaan5.java
@@ -0,0 +1,8 @@
+package experiments;
+
+public class MainPercobaan5 {
+    public static void main(String[] args) {
+        Mobil1 mobil = new Mobil1("Avanza");
+        mobil.tampilkanInfo();
+    }
+}
---
+++ jobsheet/jobsheet04/src/experiments/Mobil1.java
@@ -0,0 +1,16 @@
+package experiments;
+
+public class Mobil1 {
+    private String merek;
+    private Mesin mesin;
+    
+    public Mobil1(String merek) {
+        this.merek = merek;
+        this.mesin = new Mesin();
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Mobil: " + this.merek);
+        System.out.println("Mesin: " + this.mesin.getTipe());
+    }
+}
